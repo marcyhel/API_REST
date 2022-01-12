e98755e45ebc3e9ad98941ed14077a597c4359fb
marcyhel menezes <h.marcyhel2012@gmail.com>
2022-01-12 17:23:45 -0300

    2

--- config/conection.js
+++ config/conection.js
@@ -7,7 +7,7 @@ class Conection{
     dbConectionMongodb(){
         const keydb = process.env.KEY_MONGO || "falha na variavel";
         console.log(keydb);
-        this.mongoDBconection  = mongoose.connect(keydb,{}).then(()=>{console.log("conexão con sucesso")}).catch((erro)=>{console.log(`erro: ${erro}`)})
+        this.mongoDBconection  = mongoose.connect('mongodb+srv://marcyhel:042224@cluster0.wkuub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{}).then(()=>{console.log("conexão con sucesso")}).catch((erro)=>{console.log(`erro: ${erro}`)})
     }
 
 }
